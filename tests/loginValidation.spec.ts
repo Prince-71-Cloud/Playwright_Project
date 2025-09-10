06e24ca5453f8bfc702fe1daf9be5ebe11da1c58
aman bhuiyan <bhuiyanaman71@stud.cou.ac.bd>
2025-09-10 23:20:13 +0600

    Playing with Playwright

--- tests/loginValidation.spec.ts
+++ tests/loginValidation.spec.ts
@@ -1,145 +1,160 @@
 import { test, expect } from '@playwright/test';
 
 //@ts-check
-test('has title', async ({ page }) => {
-  await page.goto('https://practice.qabrains.com/');
+test.describe('Login Page Validation', () => {
 
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/QA Practice Site/);
+test.beforeEach(async ({ page }) => {
+  // Go to the starting url before each test.
+  await page.goto('https://aiaxio.com');
 });
 
-test('Login with valid credentials and write feedback', async ({ page }) => {
-  await page.goto('https://practice.qabrains.com/');
+test('Home page Navigation Bar visibility', async ({ page }) => {
+  const navbarLogo = page.getByTestId("navbar-logo");
+  await expect(navbarLogo).toBeVisible();
 
-  // Fill in the login form.
-  await page.fill('//input[@id="email"]', 'qa_testers@qabrains.com');
-  await page.waitForTimeout(2000);
-  await page.fill('//input[@id="password"]', 'Password123');
-  await page.waitForTimeout(2000);
+  const navbarHome = page.getByTestId("nav-link-Tools");
+  await expect(navbarHome).toBeVisible();
+  await expect(navbarHome).toHaveText('Tools');
 
-  //click login
-  await page.click('//button[@type="submit"]');
-  await page.waitForLoadState('networkidle');
+  const navbarSubmitTools = page.getByTestId("nav-link-Submit Tool");
+  await expect(navbarSubmitTools).toBeVisible();
+  await expect(navbarSubmitTools).toHaveText('Submit Tool');
 
-  // check user is logged in
-  const loginMessage = page.locator('div[class="toaster bg-white rounded-md shadow-lg inline-flex gap-2 items-center min-w-[220px] px-4 py-3"]');
-  await expect(loginMessage).toHaveText('Login Successful');
-  await page.waitForTimeout(2000);
+  const navbarSearch = page.getByTestId("nav-link-Search");
+  await expect(navbarSearch).toBeVisible();
+  await expect(navbarSearch).toHaveText('Search');
 
-  // // write Feedback
-  // const feedback = page.locator("(//textarea[@placeholder='Write Comment...'])[1]");
-  // await feedback.fill("This is a Feedback from Playwright script.");
-  // await page.click("(//button[normalize-space()='Submit'])[1]");
-  // await page.waitForTimeout(2000);
+  const navbarSignIn = page.getByRole('button', { name: 'Sign In' });
+  await expect(navbarSignIn).toBeVisible();
+  await expect(navbarSignIn).toHaveText('Sign In');
 
-  // click logout
-  await page.click("//button[normalize-space()='Logout']");
-  await page.waitForTimeout(2000);
+  const navbarSignUp = page.getByRole('button', { name: 'Sign Up' });
+  await expect(navbarSignUp).toBeVisible();
+  await expect(navbarSignUp).toHaveText('Sign Up');
+
+
 });
 
-test('Login with both invalid credentials', async ({ page }) => {
-  await page.goto('https://practice.qabrains.com/');
+test('Home page heading part visibility', async ({ page }) => {
+  await expect(page).toHaveTitle(/AI Tools/i);
 
-  // Fill in the login form.
-  await page.fill('//input[@id="email"]', 'invalid_user@qabrains.com');
-  await page.waitForTimeout(2000);
-  await page.fill('//input[@id="password"]', 'InvalidPassword');
-  await page.waitForTimeout(2000);
+  const homePageH1 = page.locator('xpath=/html[1]/body[1]/div[1]/div[1]/div[1]/div[1]/h1[1]');
+  await expect(homePageH1).toBeVisible();
+  await expect(homePageH1).toHaveText(/AIAXIO-AI Matched To Your Need/i);
+  
+  const homepageParagraph = page.locator('xpath=/html[1]/body[1]/div[1]/div[1]/div[1]/div[1]/p[1]');
+  await expect(homepageParagraph).toBeVisible();
 
-  // click login
-  await page.click('//button[@type="submit"]');
-  await page.waitForLoadState('networkidle');
+  const homePageSearchBox = page.getByTestId('tool-search-input');
+  await expect(homePageSearchBox).toBeVisible();
 
-  // check user is not logged in
-  const errorMessage = page.locator('div[class="toaster bg-white rounded-md shadow-lg inline-flex gap-2 items-center min-w-[220px] px-4 py-3"]');
-  await expect(errorMessage).toHaveText('Your email and password both are invalid!');
-  await page.waitForTimeout(2000);
+  const homepageJustLanded = page.getByTestId("filter-link-just-landed");
+  await expect(homepageJustLanded).toBeVisible();
+  await expect(homepageJustLanded).toHaveText('Just Landed');
+
+  const homepageMostPopular = page.getByTestId("filter-link-popular");
+  await expect(homepageMostPopular).toBeVisible();
+  await expect(homepageMostPopular).toHaveText('Popular');
+
+  const homepageFreemium = page.getByTestId("filter-link-freemium");
+  await expect(homepageFreemium).toBeVisible();
+  await expect(homepageFreemium).toHaveText('Freemium');
+
+  const homepageFree = page.getByTestId("filter-link-free");
+  await expect(homepageFree).toBeVisible();
+  await expect(homepageFree).toHaveText('Free');
+
+  const homepageTrending = page.getByTestId("filter-link-trending");
+  await expect(homepageTrending).toBeVisible();
+  await expect(homepageTrending).toHaveText('Trending');
+
 });
 
-test('Login with invalid Email', async ({ page }) => {
-  await page.goto('https://practice.qabrains.com/');
+test('Latest AI Tools section visibility', async ({ page }) => {
+  const latestAIToolsTitle = page.locator("xpath=//h2[normalize-space()='Latest AI Tools']");
+  await expect(latestAIToolsTitle).toBeVisible();
 
-  // Fill in the login form.
-  await page.fill('//input[@id="email"]', 'invaliduser@qabrains.com');
-  await page.waitForTimeout(2000);
-  await page.fill('//input[@id="password"]', 'Password123');
-  await page.waitForTimeout(2000);
+  const latestAIToolsViewAll = page.getByTestId("view-all-newly-released-tools");
+  await expect(latestAIToolsViewAll).toBeVisible();
 
-  // click login
-  await page.click('//button[@type="submit"]');
-  await page.waitForLoadState('networkidle');
+  const latestAIToolsCards = page.getByTestId("new-release-tools-grid");
+  await expect(latestAIToolsCards).toBeVisible();
 
-  // check user is not logged in
-  const errorMessage = page.locator('div[class="toaster bg-white rounded-md shadow-lg inline-flex gap-2 items-center min-w-[220px] px-4 py-3"]');
-  await expect(errorMessage).toHaveText('Your email is invalid!');
-  await page.waitForTimeout(2000);
+  // const latestAIToolsLoadMore = page.getByTestId("load-more-newly-released-tools");
+  // await expect(latestAIToolsLoadMore).toBeVisible();
+
 });
 
-test('Login with invalid Password', async ({ page }) => {
-  await page.goto('https://practice.qabrains.com/');
+test('AI for Education section visibility', async ({ page }) => {
+  const aiEducationTitle = page.locator("xpath=//h2[normalize-space()='AI for Education']");
+  await expect(aiEducationTitle).toBeVisible();
 
-  // Fill in the login form.
-  await page.fill('//input[@id="email"]', 'qa_testers@qabrains.com');
-  await page.waitForTimeout(2000);
-  await page.fill('//input[@id="password"]', 'Password1234');
-  await page.waitForTimeout(2000);
+  const aiEducationViewAll = page.getByTestId("view-tools-education");
+  await expect(aiEducationViewAll).toBeVisible();
 
-  // click login
-  await page.click('//button[@type="submit"]');
-  await page.waitForLoadState('networkidle');
-
-  // check user is not logged in
-  const errorMessage = page.locator('div[class="toaster bg-white rounded-md shadow-lg inline-flex gap-2 items-center min-w-[220px] px-4 py-3"]');
-  await expect(errorMessage).toHaveText('Your password is invalid!');
-  await page.waitForTimeout(2000);
+  const aiEducationCards = page.getByTestId("tools-grid-education");
+  await expect(aiEducationCards).toBeVisible();
 });
 
-test('Check Navbar Visibility', async ({ page }) => {
-  await page.goto('https://practice.qabrains.com/');
+test('AI writing Assistant section visibility', async ({ page }) => {
+  const aiWritingAssistantTitle = page.locator("xpath=//h2[normalize-space()='AI Writing Assistants']");
+  await expect(aiWritingAssistantTitle).toBeVisible();
 
-  // check if navbar logo is visible
-  const navLogo = page.locator("img[alt='Logo']");
-  await expect(navLogo).toBeVisible();
-  await page.waitForTimeout(2000);
+  const aiWritingAssistantViewAll = page.getByTestId("view-tools-writing");
+  await expect(aiWritingAssistantViewAll).toBeVisible();
 
-   // check if navbar Home link is visible
-  const navHomeLink = page.locator("(//a[normalize-space()='Home'])[1]");
-  await expect(navHomeLink).toBeVisible();
-  await page.waitForTimeout(2000);
+  const aiWritingAssistantCards = page.getByTestId("tools-grid-writing");
+  await expect(aiWritingAssistantCards).toBeVisible();
+});
 
-   // check if navbar QA link is visible
-  const navQALink = page.locator("(//a[normalize-space()='QA Topics'])[1]");
-  await expect(navQALink).toBeVisible();
-  await page.waitForTimeout(2000);
+test("AI Coding Tools section visibility", async ({ page }) => {
+  const aiCodingToolsTitle = page.locator("xpath=//h2[normalize-space()='AI Coding Tools']");
+  await expect(aiCodingToolsTitle).toBeVisible();
 
-  // check if navbar Discussion link is visible
-  const navDiscussion = page.locator("(//a[@class='inline-block'][normalize-space()='Discussion'])[1]");
-  await expect(navDiscussion).toBeVisible();
-  await page.waitForTimeout(2000);
+  const aiCodingToolsViewAll = page.getByTestId("view-tools-coding");
+  await expect(aiCodingToolsViewAll).toBeVisible();
 
-  // check if navbar Tags link is visible
-  const navTags = page.locator("(//a[normalize-space()='Tags'])[1]");
-  await expect(navTags).toBeVisible();
-  await page.waitForTimeout(2000);
+  const aiCodingToolsCards = page.getByTestId("tools-grid-coding");
+  await expect(aiCodingToolsCards).toBeVisible();
+});
 
-   // check if navbar Jobs link is visible
-  const navJobs = page.locator("(//a[normalize-space()='Jobs'])[1]");
-  await expect(navJobs).toBeVisible();
-  await page.waitForTimeout(2000);
+test.only('Footer Elements visibility', async ({ page }) => {
+  await page.evaluate(() => window.scrollTo(0, document.body.scrollHeight));
+  
+  const homepagefooter = page.locator("footer");
+  await expect(homepagefooter).toBeVisible();
 
-  // check if navbar Practice Site link is visible
-  const navPracticeSite = page.locator("(//a[normalize-space()='Practice Site'])[1]");
-  await expect(navPracticeSite).toBeVisible();
-  await page.waitForTimeout(2000);
+  const footerLinksAboutUs = homepagefooter.locator("//a[normalize-space()='About Us']");
+  await expect(footerLinksAboutUs).toBeVisible();
 
-  // check if navbar About Us link is visible
-  const navAboutUs = page.locator("(//a[@class='inline-block'][normalize-space()='About Us'])[1]");
-  await expect(navAboutUs).toBeVisible();
-  await page.waitForTimeout(2000);
+  const footerLinksContactUs = homepagefooter.locator("//a[normalize-space()='Contact Us']");
+  await expect(footerLinksContactUs).toBeVisible();
 
-  // check if navbar Sign In link is visible
-  const navSignIn = page.locator("(//a[normalize-space()='Sign In'])[1]");
-  await expect(navSignIn).toBeVisible();
-  await page.waitForTimeout(2000);
+  const footerLinksfaq = homepagefooter.locator("//a[normalize-space()='FAQ']");
+  await expect(footerLinksfaq).toBeVisible();
+
+  const footerTermsOfService = homepagefooter.locator("//a[normalize-space()='Terms of Service']");
+  await expect(footerTermsOfService).toBeVisible();
+
+  const footerPrivacyPolicy = homepagefooter.locator("//a[normalize-space()='Privacy Policy']");
+  await expect(footerPrivacyPolicy).toBeVisible();
+
+  const footerCookiesPolicy = homepagefooter.locator("//a[normalize-space()='Cookies Policy']");
+  await expect(footerCookiesPolicy).toBeVisible();
+
+  const footerDisclaimer = homepagefooter.locator("//a[normalize-space()='Disclaimer']"); 
+  await expect(footerDisclaimer).toBeVisible();
+
+  const footerLogo = homepagefooter.locator("(//div[@class='flex items-center gap-4'])[1]");
+  await expect(footerLogo).toBeVisible();
+
+  const footerSocialMedia = homepagefooter.locator("(//div[@class='flex gap-4'])[1]");
+  await expect(footerSocialMedia).toBeVisible();
+
+  // Scroll to the bottom of the page to make the scroll-to-top button visible
+  
+  const scrollToTopButton = page.getByTestId("scroll-to-top-button");
+  await expect(scrollToTopButton).toBeVisible();
+});
 
 });
